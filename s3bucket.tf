180ffe3213b0a762e4d2b165e3f2f172c2f26392
rahmath851 <96294788+rahmath851@users.noreply.github.com>
2022-02-01 11:44:36 +0530

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,3 +1,10 @@
+provider "aws" {
+region = "us-east-1"
+access_key = "AKIA5ZYFB5V63MKBZ3AV"
+secret_key = "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"
+}
+
+
 resource "aws_s3_bucket" "def" { 
 bucket = "flipkartflipkart234"
 acl = "private"
